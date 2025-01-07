cdd4f7ac0db86e0811f0a0966f3ea1be6b273e68
andressa-mb <andressamesquita26@gmail.com>
2025-01-07 16:26:27 -0300

    Cadastro de produto no banco

--- src/Leilao/dao/ProdutosDAO.java
+++ src/Leilao/dao/ProdutosDAO.java
@@ -4,12 +4,10 @@ package Leilao.dao;
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 /**
  *
  * @author Adm
  */
-
 import Leilao.model.ProdutosDTO;
 import java.sql.PreparedStatement;
 import java.sql.Connection;
@@ -17,29 +15,35 @@ import javax.swing.JOptionPane;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 
-
 public class ProdutosDAO {
-    
+
     Connection conn;
     PreparedStatement prep;
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
-    
-    public void cadastrarProduto (ProdutosDTO produto){
-        
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
+
+    public ProdutosDAO() {
+        this.conn = new conectaDAO().connectDB();
     }
-    
-    public ArrayList<ProdutosDTO> listarProdutos(){
-        
+
+    public void cadastrarProduto(ProdutosDTO produto) {
+        String sql = "INSERT INTO produtos (nome, valor, status) values (?, ?, ?)";
+        try {
+            prep = this.conn.prepareStatement(sql);
+            prep.setString(1, produto.getNome());
+            prep.setInt(2, produto.getValor());
+            prep.setString(3, produto.getStatus());
+            prep.execute();
+        } catch (Exception e) {
+            System.out.println("Erro ao inserir novo produto. " + e.getMessage());
+
+        }
+
+    }
+
+    public ArrayList<ProdutosDTO> listarProdutos() {
+
         return listagem;
     }
-    
-    
-    
-        
-}
 
+}
